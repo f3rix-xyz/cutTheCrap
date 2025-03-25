cfedc0f17b7d8b5c32de7c349202667fe9db29b3
arnnvv <arnav.900@protonmail.com>
2025-03-25 18:21:34 +0530

    connected

--- db/queries.sql
+++ db/queries.sql
@@ -3,12 +3,12 @@ INSERT INTO users (google_id, email, name, picture)
 VALUES ($1, $2, $3, $4)
 RETURNING *;
 
--- name: CreateSession :one
+-- name: CreateSession :exec
 INSERT INTO sessions (id, user_id, expires_at)
-VALUES ($1, $2, $3)
+VALUES ($1, $2, $3);
 
 -- name: GetUserByGoogleID :one
-SELECT * FROM "users"
+SELECT * FROM users
 WHERE google_id = $1
 LIMIT 1;
 
@@ -19,10 +19,10 @@ INNER JOIN users u ON s.user_id = u.id
 WHERE s.id = $1
 LIMIT 1;
 
--- name: UpdateSessionExpiration :one
+-- name: UpdateSessionExpiration :exec
 UPDATE sessions
 SET expires_at = $1
-WHERE id = $2
+WHERE id = $2;
 
 -- name: DeleteSessionByID :exec
 DELETE FROM sessions
